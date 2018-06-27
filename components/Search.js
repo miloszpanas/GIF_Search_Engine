7b62dc297640caa22c4fa9d838a74e57080f0e8e
Miłosz Panas <miloszpanas@gmail.com>
2018-06-27 14:47:46 +0200

    update3

--- components/Search.js
+++ components/Search.js
@@ -21,7 +21,7 @@ Search = React.createClass({
       },
     
     render: function() {
-        var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
+        var styles = {fontSize: '1.3em', width: '90%', maxWidth: '350px'};
     
         return <input
                  type="text"
